7af2f86e916663ecc1682ebbcb364e107a087ea1
Daniel Kurtz <djkurtz@chromium.org>
2015-12-18 16:09:02 +0800

    FIXUP: CHROMIUM: FROMLIST: clk: mediatek: Export CPU mux clocks for CPU frequency control
    Upstream __clk_get_num_parents changed to clk_cpumux_get_parent.
    
    drivers/clk/mediatek/clk-cpumux.c: In function 'clk_cpumux_get_parent':
    drivers/clk/mediatek/clk-cpumux.c:38:2: error: implicit declaration of function '__clk_get_num_parents' [-Werror=implicit-function-declaration]
      int num_parents = __clk_get_num_parents(hw->clk);
    
    Change-Id: I16300b634b1aa029d2a1f7a9511139849f5be646
    Signed-off-by: Daniel Kurtz <djkurtz@chromium.org>

--- drivers/clk/mediatek/clk-cpumux.c
+++ drivers/clk/mediatek/clk-cpumux.c
@@ -35,7 +35,7 @@ static inline struct mtk_clk_cpumux *to_clk_mux(struct clk_hw *_hw)
 static u8 clk_cpumux_get_parent(struct clk_hw *hw)
 {
 	struct mtk_clk_cpumux *mux = to_clk_mux(hw);
-	int num_parents = __clk_get_num_parents(hw->clk);
+	int num_parents = clk_hw_get_num_parents(hw);
 	unsigned int val;
 
 	regmap_read(mux->regmap, mux->reg, &val);
